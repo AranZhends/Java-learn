f6959090754862b9284daed70fdebc8b1d1b6711
AranZhends <690339590@qq.com>
2021-04-24 21:25:42 +0800

    满二叉树的最近公共祖先

---
+++ 满二叉树的最近公共祖先/src/Main.java
@@ -0,0 +1,17 @@
+public class LCA {
+    public int getLCA(int a, int b) {
+        while (a != b) {
+            if (a>b) {
+                a /= 2;
+            }else {
+                b /= 2;
+            }
+        }
+        return  a;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
