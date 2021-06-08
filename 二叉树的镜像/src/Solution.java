630bbfd735898449ec7f46e701a3b52149ac7fd4
AranZhends <690339590@qq.com>
2021-06-08 21:40:47 +0800

    二叉树的镜像

---
+++ 二叉树的镜像/src/Solution.java
@@ -0,0 +1,21 @@
+ class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+public class Solution {
+    public TreeNode Mirror (TreeNode pRoot) {
+        // write code here
+        if(pRoot == null) return null;
+        TreeNode temp = pRoot.left;
+        pRoot.left = pRoot.right;
+        pRoot.right = temp;
+        Mirror(pRoot.left);
+        Mirror(pRoot.right);
+        return pRoot;
+    }
+}
